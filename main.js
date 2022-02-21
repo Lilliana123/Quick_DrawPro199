f5b39e2a295efdfca8a264004542261626deb2a8
Lilliana123 <84448213+Lilliana123@users.noreply.github.com>
2022-02-21 16:44:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+function setup()
+{
+    canvas = createCanvas(280, 200);
+    canvas.center();
+    background("black");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+}
+
+function clearCanvas(){
+    background("black");
+}
+function preload()
+{
+    classifier = ml5.imageClassifier('DoodleNet');
+}
+
+function draw(){
+strokeWeight(10);
+stroke("white");
+if (mouseIsPressed){
+    line(pmouseX, pmouseY, mouseX, mouseY);
+}
+}
+function classifyCanvas()
+{
+    classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    console.log(results);
+    document.getElementById('label').innerHTML = ' label: ' + results[0].label;
+
+    document.getElementById('confidence').innerHTML = ' Confidence: ' + Math.round(results[0].confidence * 100) + '%';
+
+    utterThis = new SpeechSynthesisUtterance(results[0].label);
+    synth.speak(utterThis);
+}
